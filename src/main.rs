da7244292dd87d19c8aff8ee2ab50a5bf5614ab7
yubo <yubo@rivtower.com>
2021-06-09 09:31:44 +0800

    fix authority bug

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,10 @@
-use bincode;
-use cita_types;
-use clap;
 use common_types as types;
-
 use types::db_indexes::{
     BlockNumber2Hash, BlockNumber2Header, CurrentHash, CurrentProof, DBIndex, Hash2Header,
 };
 
-use cita_types::H256;
+use authority_manage::AuthorityManage;
+use cita_types::{Address, H256};
 use clap::App;
 
 use cita_database::{Config, DataCategory, Database, RocksDB, NUM_COLUMNS};
@@ -27,10 +24,10 @@ use proof::BftProof;
 use std::fs::{read_dir, remove_file, OpenOptions};
 use std::io::{self, Read, Seek, Write};
 use std::mem::transmute;
-//use types::db::Key;
 
 const PTYPE: u8 = 5;
 const HTYPE: u8 = 4;
+const DATA_PATH: &str = "DATA_PATH";
 const WAL: &str = "/wal";
 const NOSQL: &str = "/nosql";
 const STATEDB: &str = "/statedb";
@@ -38,23 +35,28 @@ const STATEDB: &str = "/statedb";
 fn delete_higher_log_file(data_path: &str, height: usize) {
     let wal_path = data_path.to_string() + WAL;
     if let Ok(entries) = read_dir(wal_path.clone()) {
-        for entry in entries {
-            if let Ok(entry) = entry {
-                // Here, `entry` is a `DirEntry`.
-                if let Ok(fname) = entry.file_name().into_string() {
-                    let vec_str: Vec<&str> = fname.split(".log").collect();
-                    if !vec_str.is_empty() {
-                        let hi = vec_str[0].parse::<usize>().unwrap_or(0);
-                        if hi > height {
-                            let del_file = wal_path.clone() + "/" + &fname;
-                            println!("del file name {:?}", del_file);
-                            let _ = remove_file(del_file);
-                        }
+        for entry in entries.flatten() {
+            // Here, `entry` is a `DirEntry`.
+            if let Ok(fname) = entry.file_name().into_string() {
+                let vec_str: Vec<&str> = fname.split(".log").collect();
+                if !vec_str.is_empty() {
+                    let hi = vec_str[0].parse::<usize>().unwrap_or(0);
+                    if hi > height {
+                        let del_file = wal_path.clone() + "/" + &fname;
+                        println!("del file name {:?}", del_file);
+                        let _ = remove_file(del_file);
                     }
                 }
             }
         }
     }
+}
+
+fn fix_authority_log(data_path: &str, height: usize, old_validators: Vec<Address>) {
+    std::env::set_var(DATA_PATH, data_path);
+    let mut auth = AuthorityManage::new();
+    println!("fix_authority_log old authoritys -- {:?}", old_validators);
+    auth.receive_authorities_list(height - 1, &old_validators, &old_validators);
 }
 
 fn fix_wal_index(data_path: &str, height: usize) -> Result<usize, io::Error> {
@@ -176,7 +178,6 @@ fn fix_executor_db(data_path: &str, dst_height: u64) -> bool {
 
 fn fix_chain_db(data_path: &str, dst_height: u64) -> bool {
     let chain_path = data_path.to_string() + NOSQL;
-
     if !Path::new(&chain_path).exists() {
         println!("chain db dir not exist");
         return false;
@@ -239,12 +240,6 @@ fn fix_chain_db(data_path: &str, dst_height: u64) -> bool {
         result[0] = extras::ExtrasIndex::BlockBodyHash as u8;
         let yy  = chain_db.get(db::COL_BODIES, &result).unwrap().unwrap();
         println!("len get bodys {:?}",yy);
-
-        let proof = dst_header.proof();
-        let btf_proof = BftProof::from(proof.clone());
-
-        println!("btf proof -- {:?}" ,btf_proof);
-
     }*/
 
     let hash_key = BlockNumber2Header(dst_height + 1).get_index();
@@ -268,6 +263,8 @@ fn fix_chain_db(data_path: &str, dst_height: u64) -> bool {
         let pmsg = serialize(&btf_proof, Infinite).unwrap();
         let hmsg = dst_hash.to_vec();
         let _ = fix_height_log(data_path, dst_height, pmsg, hmsg);
+        let validators: Vec<Address> = btf_proof.commits.keys().cloned().collect();
+        fix_authority_log(data_path, dst_height as usize, validators);
     }
 
     //println!("header is {:?}", dst_header);
